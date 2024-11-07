e0d733db1a195d84531a3d9cfb38ac25f1aaac42
Alex Jamarco <a.jamarco@gmail.com>
2024-11-07 13:05:39 +0000

    Added mock data for hotels and added the fetch data on App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import hotelData from "./hotelData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [hotels, setHotels] = useState([]);
+  const [sortMethod, setSortMethod] = useState("price");
+
+  useEffect(() => {
+    async function fetchHotels() {
+      try {
+        const data = hotelData;
+        console.log(data);
+        setHotels(data);
+      } catch (error) {
+        console.error("error fetching hotel data", error);
+      }
+    }
+    fetchHotels();
+  }, []);
+
+  return <div>App</div>;
+};
 
 export default App;
