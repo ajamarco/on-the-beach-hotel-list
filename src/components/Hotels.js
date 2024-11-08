f924df918eaa893da2689b4593faf67eefd8c95c
Alex Jamarco <a.jamarco@gmail.com>
2024-11-08 06:58:31 +0000

    Added HotelCard properties when called in Hotels.js

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -2,7 +2,14 @@ import React from "react";
 import HotelCard from "./HotelCard";
 
 const Hotels = ({ hotels }) => {
-  return hotels.map((hotel) => <HotelCard hotel={hotel} />);
+  return hotels.map((hotel) => (
+    <HotelCard
+      hotel={hotel?.resort}
+      id={hotel?.resort.id}
+      flight={hotel?.flightDetails}
+      bookingDetails={hotel?.bookingDetails}
+    />
+  ));
 };
 
 export default Hotels;
