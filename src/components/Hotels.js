f99a63b78fa613230dd2a25aa6d74656c871de33
Alex Jamarco <a.jamarco@gmail.com>
2024-11-08 07:15:39 +0000

    Added a wrap section to Hotels and moved the map logic to a different function

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -2,14 +2,18 @@ import React from "react";
 import HotelCard from "./HotelCard";
 
 const Hotels = ({ hotels }) => {
-  return hotels.map((hotel) => (
-    <HotelCard
-      hotel={hotel?.resort}
-      id={hotel?.resort.id}
-      flight={hotel?.flightDetails}
-      bookingDetails={hotel?.bookingDetails}
-    />
-  ));
+  const renderHotelCards = () => {
+    return hotels.map((hotel) => (
+      <HotelCard
+        hotel={hotel?.resort}
+        id={hotel?.resort.id}
+        flight={hotel?.flightDetails}
+        bookingDetails={hotel?.bookingDetails}
+      />
+    ));
+  };
+
+  return <section className="trips">{renderHotelCards()}</section>;
 };
 
 export default Hotels;
