18bd1efe751db648a82720dcbd8c3ef96f54b753
Alex Jamarco <a.jamarco@gmail.com>
2024-11-10 11:00:05 +0000

    Added key param to Hotels and used useMemo to avoid unnecessary rendering.

--- src/components/Hotels.js
+++ src/components/Hotels.js
@@ -1,19 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
 import HotelCard from "./HotelCard";
 
 const Hotels = ({ hotels }) => {
-  const renderHotelCards = () => {
+  const renderHotelCards = useMemo(() => {
     return hotels.map((hotel) => (
       <HotelCard
-        hotel={hotel?.resort}
-        id={hotel?.resort.id}
-        flight={hotel?.flightDetails}
-        bookingDetails={hotel?.bookingDetails}
+        hotel={hotel.resort}
+        id={hotel.resort.id}
+        flight={hotel.flightDetails}
+        bookingDetails={hotel.bookingDetails}
+        key={hotel.resort.id}
       />
     ));
-  };
+  }, [hotels]);
 
-  return <section className="trips">{renderHotelCards()}</section>;
+  return <section className="trips">{renderHotelCards}</section>;
 };
 
 export default Hotels;
