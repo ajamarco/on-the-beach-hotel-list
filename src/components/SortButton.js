f6f1b2f9855dbfc76fc3baffac0a2691ff58ed43
Alex Jamarco <a.jamarco@gmail.com>
2024-11-10 11:58:55 +0000

    Moved the icon logic to the Sort button, as well added the grey colour for inactive sort buttons

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -1,9 +1,37 @@
+import { MdSortByAlpha } from "react-icons/md";
+import { AiFillPoundCircle } from "react-icons/ai";
+import { FaStar } from "react-icons/fa";
+
 import "../styles/SortButton.scss";
 
-const SortButton = ({ active, clickHandler, SortBy, icon }) => {
+const SortButton = ({ active, clickHandler, SortBy }) => {
   // Handle click events on the sort button. This was extracted from the return so we could add more functionalities in the future
   const handleClick = () => {
     clickHandler(SortBy);
+  };
+
+  // Map the SortBy value to the corresponding icon and we set a class based on the active state
+  const getIcon = (SortBy, active) => {
+    switch (SortBy) {
+      case "Name":
+        return (
+          <MdSortByAlpha
+            className={active ? "activeSortIcon" : "inactiveSortIcon"}
+          />
+        );
+      case "Price":
+        return (
+          <AiFillPoundCircle
+            className={active ? "activeSortIcon" : "inactiveSortIcon"}
+          />
+        );
+      case "Star Rating":
+        return (
+          <FaStar className={active ? "activeSortIcon" : "inactiveSortIcon"} />
+        );
+      default:
+        return null;
+    }
   };
 
   return (
@@ -14,7 +42,7 @@ const SortButton = ({ active, clickHandler, SortBy, icon }) => {
       <p>
         Sort by <span>{SortBy}</span>
       </p>
-      {icon}
+      {getIcon(SortBy)}
     </div>
   );
 };
--- src/components/SortSelection.js
+++ src/components/SortSelection.js
@@ -1,20 +1,9 @@
-import { MdSortByAlpha } from "react-icons/md";
-import { AiFillPoundCircle } from "react-icons/ai";
-import { FaStar } from "react-icons/fa";
-
 import "../styles/SortSelection.scss";
 import SortButton from "./SortButton";
 
 const SortSelection = ({ activeSort, handleSortChange }) => {
   // Define the available sort options
   const sortOptions = ["Name", "Price", "Star Rating"];
-
-  // create an object to store the icons for each sort option
-  const sortIcons = {
-    Name: <MdSortByAlpha />,
-    Price: <AiFillPoundCircle />,
-    "Star Rating": <FaStar />,
-  };
 
   // Handle click events on sort buttons
   const clickHandler = (sortBy) => {
@@ -30,7 +19,6 @@ const SortSelection = ({ activeSort, handleSortChange }) => {
           key={index}
           active={option === activeSort}
           clickHandler={clickHandler}
-          icon={sortIcons[option]}
         />
       ))}
     </aside>
